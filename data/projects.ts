6f710916a5500edc6feb96d3a6ed8cf0c91874f1
Andrés Sánchez <andsancapdev@gmail.com>
2026-06-05 14:16:16 +0200

    Añado url proyecto

--- data/projects.ts
+++ data/projects.ts
@@ -4,7 +4,7 @@ export const projects = [
     description:
       "Platform to manage, stream, and distribute guided audio sessions. Features include user authentication, content management, analytics dashboard, and real-time streaming.",
     stack: ["Next.js", "Supabase", "Expo"],
-    link: "#",
+    link: "https://www.comunidadesmeralda.com",
     image: "/projects/project1.svg",
     featured: true,
   },
